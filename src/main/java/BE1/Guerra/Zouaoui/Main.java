62fd8dedad03dcf1dd4ca05aac2b11fec5b2981b
zouaoui <ilies.zouaoui@ecl19.ec-lyon.fr>
2020-09-17 11:38:37 +0200

    Création des fichiers de base du projet

---
+++ src/main/java/BE1/Guerra/Zouaoui/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BE1.Guerra.Zouaoui;
+
+/**
+ *
+ * @author iliès
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
